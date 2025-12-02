529c829aa5af2382d7feb679d511fbbc98561179
maratfront <marat.frontend@gmail.com>
2025-12-02 19:29:49 +0300

    Исправлен батник

--- start.bat
+++ start.bat
@@ -1,22 +1,6 @@
 @echo off
-chcp 65001 >nul
+REM Запуск Django сервера
+start "Django Server" cmd /k "cd server && if not exist venv ( python -m venv venv ) && call venv\Scripts\activate && pip install -r requirements.txt && python manage.py runserver"
 
-rem Путь к корню проекта (папка, где лежит этот .bat)
-set PROJECT_ROOT=%~dp0
-
-rem --- Консоль для БЭКЕНДА ---
-start "Diploma Backend" cmd /k cd /d %PROJECT_ROOT%server ^& python -m venv .venv ^& call .venv\Scripts\activate.bat ^& pip install -r requirements.txt ^& python manage.py runserver
-
-rem --- Консоль для ФРОНТЕНДА ---
-start "Diploma Frontend" cmd /k cd /d %PROJECT_ROOT%client ^& npm install ^& npm run dev
-
-echo.
-echo Открыты два окна:
-echo   - Diploma Backend  (установка и запуск Django)
-echo   - Diploma Frontend (установка и запуск React)
-echo.
-echo Для остановки приложения закройте эти окна.
-echo.
-pause
-
-
+REM Запуск Vite клиента
+start "Vite Client" cmd /k "cd client && npm install && npm run dev"
