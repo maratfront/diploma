949a39c4682b7f0778c898464a03c6bc9ca6f9e1
maratfront <marat.frontend@gmail.com>
2025-12-02 21:02:40 +0300

    Фиксы фронтенда

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Запуск Django сервера
-start "Django Server" cmd /k "cd server && if not exist venv ( python -m venv venv ) && call venv\Scripts\activate && pip install -r requirements.txt && python manage.py runserver"
+start "Django Server" cmd /k "cd server && if exist venv ( call venv\Scripts\activate && python manage.py runserver ) else ( python -m venv venv && call venv\Scripts\activate && pip install -r requirements.txt && python manage.py runserver )"
 
 REM Запуск Vite клиента
 start "Vite Client" cmd /k "cd client && npm install && npm run dev"
